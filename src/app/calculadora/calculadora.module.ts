922cab9e154fd9260b23014823e175b97c9c22aa
gean634n <gean.a.l.c@gmail.com>
2022-12-14 17:37:49 -0400

    cria o componente calculadora e faz a renderização do mesmo

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CalculadoraComponent } from './components';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalculadoraComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CalculadoraComponent
   ]
 })
 export class CalculadoraModule { }
