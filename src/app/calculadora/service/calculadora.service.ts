29c8236bfc9f3d28a5589c41a2b729de57bee0a6
gean634n <gean.a.l.c@gmail.com>
2022-12-14 19:39:08 -0400

    cria o metodo calcular na calculadora.service

--- src/app/calculadora/service/calculadora.service.ts
+++ src/app/calculadora/service/calculadora.service.ts
@@ -5,5 +5,36 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  // Define as constantes utilizadas para identificar as operações de cálculo
+  static readonly SOMA: string = '+';
+  static readonly SUBTRAÇÃO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  // Método que faz o calculo das operações matemáticas
+  calcular(num1: number, num2: number, operacao: string): number {
+    let result: number; // armazena o resultado da operação
+
+    switch (operacao) {
+      case CalculadoraService.SOMA:
+        result = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRAÇÃO:
+        result = num1 - num2;
+        break;
+      case CalculadoraService.DIVISAO:
+        result = num1 / num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        result = num1 * num2;
+        break;
+      default:
+        result = 0;
+        break;
+    }
+
+    return result;
+  }
 }
