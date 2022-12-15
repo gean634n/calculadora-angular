8edce204d9eec2af3dd75c5435750563e9fdb609
gean634n <gean.a.l.c@gmail.com>
2022-12-15 05:55:10 -0400

    test: cria os testes unitarios para calculadora.service

--- src/app/calculadora/service/calculadora.service.spec.ts
+++ src/app/calculadora/service/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -13,4 +13,33 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('deve garantir que 1 + 4 = 5', inject([CalculadoraService], (service: CalculadoraService) => {
+    let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+    expect(soma).toEqual(5);
+  }));
+
+  it('deve garantir que 1 - 5 = -4', inject([CalculadoraService], (service: CalculadoraService) => {
+    let subtracao = service.calcular(1, 5, CalculadoraService.SUBTRAÇÃO);
+    expect(subtracao).toEqual(-4);
+  }));
+
+  it('deve garantir que 10 / 4 = 2.5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let divisao = service.calcular(10, 4, CalculadoraService.DIVISAO);
+      expect(divisao).toEqual(2.5);
+    })
+  );
+
+  it('deve garantir que 5 * 3 = 15', inject([CalculadoraService], (service: CalculadoraService) => {
+    let multiplicacao = service.calcular(5, 3, CalculadoraService.MULTIPLICACAO);
+    expect(multiplicacao).toEqual(15);
+  }));
+
+  it('deve retornar 0 no caso de operação invalida',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let operacaoInvalida = service.calcular(5, 5, '//');
+      expect(operacaoInvalida).toEqual(0);
+    })
+  );
 });
