2111921c89ff195b49732a2745f692eff60931fa
gean634n <gean.a.l.c@gmail.com>
2022-12-15 05:11:24 -0400

    docs: adiciona documentação à calculadora.service

--- src/app/calculadora/service/calculadora.service.ts
+++ src/app/calculadora/service/calculadora.service.ts
@@ -1,8 +1,16 @@
+/**
+ * Serviço responsavel por executal as operações da calculadora
+ *
+ * @author Gean C. B. Gonçalves <https://github.com/gean634n>
+ * @since 1.0.0
+ */
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CalculadoraService {
 
   // Define as constantes utilizadas para identificar as operações de cálculo
@@ -13,7 +21,14 @@ export class CalculadoraService {
 
   constructor() { }
 
-  // Método que faz o calculo das operações matemáticas
+  /**
+   * Método que calcula uma operação matemética dado dois numeros
+   * As operações suportadas são soma, subtração, divisão e multiplicação
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @returns number Resultado da operação
+   */
   calcular(num1: number, num2: number, operacao: string): number {
     let result: number; // armazena o resultado da operação
 
@@ -32,7 +47,6 @@ export class CalculadoraService {
         break;
       default:
         result = 0;
-        break;
     }
 
     return result;
